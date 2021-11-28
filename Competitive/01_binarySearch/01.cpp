c82e4ba0fcf0048875c3b2a91e7a77c1319f549d
MATEMATNKx <minenkovvu@gmail.com>
2021-11-28 14:34:32 +0300

    A1

--- Competitive/01_binarySearch/01.cpp
+++ Competitive/01_binarySearch/01.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
 
 using namespace std;
-
+//receives an ordered array as input
 int main(){
-    cout << "Hello World" << endl;
+    int N, K;
+    cin >> N >> K;
+    int arrayN[N];
+    int arrayK[K];
+    //filled both arrays
+    for (int i = 0; i < N; ++i){
+        cin >> arrayN[i];
+    }
+    for (int i = 0; i < K; ++i){
+        cin >> arrayK[i];
+    }
+    int left = -1;
+    int right = N;
+    int mid = 0;
+
+    for (int i = 0; i < K; ++i){
+      left = -1;
+      right = N;
+      while(left < right - 1){
+        mid = (left + right )/ 2;
+        if (arrayN[mid] <= arrayK[i]){
+            left = mid;
+        }
+        else{
+            right = mid;
+        }
+      }
+    if (left == -1){
+        cout << "NO" << endl;
+    }
+    else{
+        if (arrayN[left] == arrayK[i]){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+}
 return 0;
 }
